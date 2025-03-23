bca6f99934a61141147bc1bc32f09ed7da057a50
LaaGi-2 <parahment.prtgl20@gmail.com>
2025-03-23 16:59:31 +0700

    sidebar fix

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -44,48 +44,48 @@
         <div class="sidebar-menu">
             <ul class="menu">
                 <li class="sidebar-title">Menu</li>
-
-                <li class="sidebar-item active ">
-                    <a href="{{route('home')}}" class='sidebar-link'>
+            
+                <li class="sidebar-item {{ request()->routeIs('home') ? 'active' : '' }}">
+                    <a href="{{ route('home') }}" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
                 </li>
-
-                <li class="sidebar-item">
-                    <a href="{{route('rent.index')}}" class='sidebar-link'>
+            
+                <li class="sidebar-item {{ request()->routeIs('rent.index') ? 'active' : '' }}">
+                    <a href="{{ route('rent.index') }}" class='sidebar-link'>
                         <i class="bi bi-car-front-fill"></i>
                         <span>Sewa Mobil</span>
                     </a>
                 </li>
-
-                <li class="sidebar-item">
-                    <a href="{{route('galery.index')}}" class='sidebar-link'>
+            
+                <li class="sidebar-item {{ request()->routeIs('galery.index') ? 'active' : '' }}">
+                    <a href="{{ route('galery.index') }}" class='sidebar-link'>
                         <i class="bi bi-images"></i>
                         <span>Galery</span>
                     </a>
                 </li>
-
-                <li class="sidebar-item">
-                    <a href="{{route('review.index')}}" class='sidebar-link'>
+            
+                <li class="sidebar-item {{ request()->routeIs('review.index') ? 'active' : '' }}">
+                    <a href="{{ route('review.index') }}" class='sidebar-link'>
                         <i class="bi bi-chat"></i>
                         <span>Review</span>
                     </a>
                 </li>
-
+            
                 <li class="sidebar-item">
                     <a href="{{ route('logout') }}" class='sidebar-link' onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                             document.getElementById('logout-form').submit();">
                         <i class="bi bi-box-arrow-right"></i>
                         <span>{{ __('Logout') }}</span>
                     </a>
-
+            
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                     </form>
                 </li>
-
             </ul>
+            
         </div>
     </div>
 </div>
