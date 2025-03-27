da5c5f7005b4577d31a1890347ca0ea3c9c5bf45
nasyasrv <asrivanasya0@gmail.com>
2025-03-26 20:15:56 -0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login - Mazer Admin Dashboard</title>
+        <title>Login Admin</title>
         <link rel="stylesheet" href="{{asset('admin/assets/css/main/app.css')}}">
         <link rel="stylesheet" href="{{asset('admin/assets/css/pages/auth.css')}}">
-        <link rel="shortcut icon" href="{{asset('admin/assets/images/logo/favicon.svg')}}" type="image/x-icon">
-        <link rel="shortcut icon" href="{{asset('admin/assets/images/logo/favicon.png')}}" type="image/png">
+        <link rel="shortcut icon" href="{{ asset('landing/images/Nicon.png') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('landing/images/Nicon.png') }}" type="image/png">
     </head>
 
 <body>
